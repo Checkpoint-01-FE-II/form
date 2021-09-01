ca3046a9ab7bfe2e29bc23b7648687c6d45ea5fd
pedroisb <pedroisb28@gmail.com>
2021-09-01 10:47:25 -0300

    "primeiro commit"

---
+++ js/app.js
@@ -0,0 +1,26 @@
+//div macro
+    //form
+        //h1
+        //3x fieldset
+        //1x button
+
+//criação inicial dos elementos, já 
+const divPrincipal =  document.createElement("div");
+const formulario = document.createElement("form");
+const tituloForm = document.createElement("h1");
+const campoForm = document.createElement("fieldset"); //devemos criar variáveis para fieldset ou seria possível reaproveitá-la?
+const btnForm = document.createElement("button");
+
+
+//
+
+
+//apensando elementos
+document.body.appendChild(divPrincipal);
+div.appendChild(formulario);
+formulario.appendChild(tituloForm);
+formulario.appendChild(campoForm);
+formulario.appendChild(btnForm);
+
+
+
