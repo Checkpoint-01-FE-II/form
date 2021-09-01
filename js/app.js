4bd196c8858f28c73424d6c7ef09d51d44bd0767
pedroisb <pedroisb28@gmail.com>
2021-09-01 11:02:56 -0300

    edições iniciais nos elementos

--- js/app.js
+++ js/app.js
@@ -5,21 +5,37 @@
         //1x button
 
 //criação inicial dos elementos, já 
-const divPrincipal =  document.createElement("div");
+const divPrincipal = document.createElement("div");
 const formulario = document.createElement("form");
 const tituloForm = document.createElement("h1");
-const campoForm = document.createElement("fieldset"); //devemos criar variáveis para fieldset ou seria possível reaproveitá-la?
+const campoForm1 = document.createElement("fieldset"); //devemos criar variáveis para fieldset ou seria possível reaproveitá-la?
+const campoForm2 = document.createElement("fieldset");
+const campoForm3 = document.createElement("fieldset");
 const btnForm = document.createElement("button");
 
 
-//
+//editando elementos
+divPrincipal.classList.add("caixaForm");
+    tituloForm.innerText="Formulário";
+    
+    campoForm1.setAttribute("type", "text");
+    campoForm1.setAttribute("placeholder", "Título");
+    campoForm2.setAttribute("type", "text");
+    campoForm2.setAttribute("placeholder", "Descrição");
+    campoForm1.setAttribute("type", "text");
+    campoForm1.setAttribute("placeholder", "URL da Imagem");
+
+    btnForm.setAttribute("type", "submit");
+    btnForm.innerText="Enviar";
 
 
 //apensando elementos
 document.body.appendChild(divPrincipal);
 divPrincipal.appendChild(formulario);
 formulario.appendChild(tituloForm);
-formulario.appendChild(campoForm);
+formulario.appendChild(campoForm1);
+formulario.appendChild(campoForm2);
+formulario.appendChild(campoForm3);
 formulario.appendChild(btnForm);
 
 
