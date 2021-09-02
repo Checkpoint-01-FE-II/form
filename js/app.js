5896fc7219d7e24f74851e8a85173366e92b70f8
pedroisb <pedroisb28@gmail.com>
2021-09-02 12:14:20 -0300

    formulario ganhou corpo

--- js/app.js
+++ js/app.js
@@ -1,42 +1,161 @@
+//MAPA SIMPLIFICADO
 //div macro
     //form
         //h1
         //3x fieldset
         //1x button
 
-//criação inicial dos elementos, já 
-const divPrincipal = document.createElement("div");
+//-----------------------------------------------------------------        
+//CRIAÇÃO ELEMENTOS - JÁ COM VARIÁVEIS
+
 const formulario = document.createElement("form");
-const tituloForm = document.createElement("h1");
-const campoForm1 = document.createElement("fieldset"); //devemos criar variáveis para fieldset ou seria possível reaproveitá-la?
-const campoForm2 = document.createElement("fieldset");
-const campoForm3 = document.createElement("fieldset");
-const btnForm = document.createElement("button");
+    const tituloForm = document.createElement("h1");
 
+    const campoForm1 = document.createElement("fieldset");
+        const legendCampo1 = document.createElement("legend");
+        const inputCampo1 = document.createElement("input");
+    
+    const campoForm2 = document.createElement("fieldset");
+        const legendCampo2 = document.createElement("legend");
+        const inputCampo2 = document.createElement("input");
+        
+    
+    const campoForm3 = document.createElement("fieldset");
+        const legendCampo3 = document.createElement("legend");    
+        const inputCampo3 = document.createElement("input");
 
-//editando elementos
-divPrincipal.classList.add("caixaForm");
-    tituloForm.innerText="Formulário";
+    const btnForm = document.createElement("button");
+
+//----------------------------------------------------------------- 
+//EDITANDO ATRIBUTOS DOS ELEMENTOS
+
+formulario.classList.add("nes-container");
+formulario.classList.add("is-dark");
+    
+    tituloForm.insertAdjacentText("afterbegin", "Formulário");
     
     campoForm1.setAttribute("type", "text");
-    campoForm1.setAttribute("placeholder", "Título");
+    //campoForm1.setAttribute("placeholder", "Título");    
+        legendCampo1.insertAdjacentText("afterbegin", "Título");    
+        
+        inputCampo1.setAttribute("maxlength", "25");
+        
     campoForm2.setAttribute("type", "text");
-    campoForm2.setAttribute("placeholder", "Descrição");
-    campoForm1.setAttribute("type", "text");
-    campoForm1.setAttribute("placeholder", "URL da Imagem");
+    //campoForm2.setAttribute("placeholder", "Descrição");
+        legendCampo2.insertAdjacentText("afterbegin", "Descrição");
+        
+        inputCampo2.setAttribute("maxlength", "25");
+        //MUDAR DE INPUT PARA TEXT AREA
+    
+    campoForm3.setAttribute("type", "file"); //ESTUDAR HIPÓTESE DE TER 2 TIPOS DE INPUT DE IMAGEM: FILE E TEXT (URL)
+    //campoForm3.setAttribute("placeholder", "URL da Imagem");
+        legendCampo3.insertAdjacentText("afterbegin", "Imagem");
+        //LABEL PARA RESOLVER?
+        inputCampo3.setAttribute("type", "file")
+        inputCampo3.setAttribute("accept", ".jpg, .png, .svg, .bmp")
+        
+        //inputCampo3.setAttribute("maxlength", "25"); PENSAR QUE IMG PODE TEM MUITO CARACTERE EM SUA URL.
 
     btnForm.setAttribute("type", "submit");
-    btnForm.innerText="Enviar";
+    btnForm.insertAdjacentText("afterbegin", "Enviar");
 
 
-//apensando elementos
-document.body.appendChild(divPrincipal);
-divPrincipal.appendChild(formulario);
-formulario.appendChild(tituloForm);
-formulario.appendChild(campoForm1);
-formulario.appendChild(campoForm2);
-formulario.appendChild(campoForm3);
-formulario.appendChild(btnForm);
+//----------------------------------------------------------------- 
+//APENSANDO ELEMENTOS
 
+document.body.appendChild(formulario);
+           
+    formulario.appendChild(tituloForm);
+    
+    formulario.appendChild(campoForm1);
+        campoForm1.appendChild(legendCampo1);
+        campoForm1.appendChild(inputCampo1);
+    
+    formulario.appendChild(campoForm2);
+        campoForm2.appendChild(legendCampo2);
+        campoForm2.appendChild(inputCampo2);
+    
+    formulario.appendChild(campoForm3);
+        campoForm3.appendChild(legendCampo3);
+        campoForm3.appendChild(inputCampo3);
+    
+    formulario.appendChild(btnForm);
 
+//----------------------------------------------------------------- 
+//STYLE
 
+document.querySelector("*").style.cssText = `
+    padding: 0;
+    margin: 0;
+    font-size: 10px;
+    box-sizing: border-box;
+`
+
+    formulario.style.cssText=`
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 4.5%;
+        height: 70vh;
+    `
+
+        campoForm1.style.cssText=`
+            border: 3px solid #FFC9725B;
+            width: 85%;
+        `
+            
+        //width: fit-content -> não rola com Firefox Dev Ed.
+            legendCampo1.style.cssText=`
+                display: inline-block;
+                width: fit-content;
+                margin-left: 15px;
+            `
+
+            inputCampo1.style.cssText=`
+                border: none;    
+                outline: none;
+                background-color: #212529;
+                padding: 10px 20px 20px 20px;
+                width: 95%;
+                color: #FFFFFF;
+            `
+
+        campoForm2.style.cssText=`
+            border: 3px solid #FFC9725B;
+            width: 85%;
+        `
+            
+            legendCampo2.style.cssText=`
+                display: inline-block;
+                width: fit-content;
+                margin-left: 15px;
+            `
+
+            inputCampo2.style.cssText=`
+                border: none;    
+                outline: none;
+                background-color: #212529;
+                padding: 10px 20px 20px 20px;
+                width: 95%;
+                color: #FFFFFF;
+            `
+
+        campoForm3.style.cssText=`
+            border: 3px solid #FFC9725B;
+            width: 85%;
+        `
+            
+            legendCampo3.style.cssText=`
+                display: inline-block;
+                width: fit-content;
+                margin-left: 15px;
+            `
+
+            inputCampo3.style.cssText=`
+                border: none;    
+                outline: none;
+                background-color: #212529;
+                padding: 10px 20px 20px 20px;
+                width: 95%;
+                color: #FFFFFF;
+            `
