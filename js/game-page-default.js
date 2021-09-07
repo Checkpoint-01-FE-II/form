abf4b39d368d8fb8b7428a332a9302d25cbb626d
marcelo miyoshi <miyoshi_marcelo@hotmail.com>
2021-09-06 21:01:19 -0300

    timer , botao iniciar e  botao reiniciar

--- js/game-page-default.js
+++ js/game-page-default.js
@@ -1,12 +1,52 @@
-const $header= document.querySelector('header');
+const $header = document.querySelector('header');
 const $containerCartas = document.querySelector('.container-cartas');
 
-let tempo = '00:00:00';
+let hh = 0;
+let mm = 0;
+let ss = 0;
+let cron;
+let tempo = 1000;/*quantos milessimos equivalem a 1 seg*/
 let paresEncontrados = 0;
 
+function start() {
+    cron = setInterval(() => { timer(); }, tempo)
+    document.getElementById("btniniciar").style.display = "none"
+    document.getElementById("btnreiniciar").style.display = "block"
+}
+
+function pausar() {
+
+
+}
+function recomecar() {
+    cron = clearInterval
+    hh = 0;
+    mm = 0;
+    ss = 0;
+    
+    embaralharCartas()
+    cron = setInterval(() => { timer(); }, tempo)
+}
+function timer() {
+    ss++;
+
+    if (ss == 60) {
+        ss = 0;
+        mm++;
+        if (mm == 60) {
+            mm = 0;
+            hh++;
+
+        }
+    }
+
+    let formato = "Tempo : " + (hh < 10 ? "0" + hh : hh) + ":" + (mm < 10 ? "0" + mm : mm) + ":" + (ss < 10 ? "0" + ss : ss)
+    document.getElementById("Iniciar").innerText = formato
+}
+
 $header.insertAdjacentHTML("beforeend", `
-    <h6>Tempo: ${tempo}</h6>
-    <h6>Pares encontrados: ${paresEncontrados}/7</h6>
+    <h6 id="Iniciar">Tempo : 00:00:00</h6>
+    <h6>Pares encontrados: ${paresEncontrados}/6</h6>
 `);
 
 
@@ -85,11 +125,11 @@ let cartasDefault = [
     }
 ];
 
-const embaralharCartas = (arr)=>{
-    let novoArray=[];
-    while(novoArray.length !== arr.length){
-        let i=Math.floor(Math.random()*arr.length);
-        novoArray.indexOf(arr[i])<0 ? novoArray.push(arr[i]) : '';
+const embaralharCartas = (arr) => {
+    let novoArray = [];
+    while (novoArray.length !== arr.length) {
+        let i = Math.floor(Math.random() * arr.length);
+        novoArray.indexOf(arr[i]) < 0 ? novoArray.push(arr[i]) : '';
     };
     return novoArray;
 };
@@ -97,10 +137,10 @@ const embaralharCartas = (arr)=>{
 cartasDefault = embaralharCartas(cartasDefault);
 console.log(cartasDefault)
 
-for(let i=0; i<cartasDefault.length;i++){
-    cartasDefault[i].id=`carta${i}`
-    $containerCartas.insertAdjacentHTML('beforeend', 
-    `
+for (let i = 0; i < cartasDefault.length; i++) {
+    cartasDefault[i].id = `carta${i}`
+    $containerCartas.insertAdjacentHTML('beforeend',
+        `
         <div class='cartas' id=${cartasDefault[i].id}>
             <div class='div-frente-verso front'>
                 <img class='img-cartas' src='../imgs/${cartasDefault[i].url}' alt="">
@@ -110,15 +150,15 @@ for(let i=0; i<cartasDefault.length;i++){
             </div>
         </div>
     `)
-    }
+}
 
 
-    for(let i=0; i<cartasDefault.length;i++){
-        document.querySelector("#carta" + (i)).addEventListener('click', function (){
-            let faces = this.getElementsByClassName('div-frente-verso')
-            faces[0].classList.toggle('flip');
-            faces[1].classList.toggle('flip');
-            document.querySelector('.section-descricao').innerHTML =
+for (let i = 0; i < cartasDefault.length; i++) {
+    document.querySelector("#carta" + (i)).addEventListener('click', function () {
+        let faces = this.getElementsByClassName('div-frente-verso')
+        faces[0].classList.toggle('flip');
+        faces[1].classList.toggle('flip');
+        document.querySelector('.section-descricao').innerHTML =
             `
             <img class='img-descricao'
             src="../imgs/${cartasDefault[i].url}"
@@ -128,6 +168,6 @@ for(let i=0; i<cartasDefault.length;i++){
                 <p>${cartasDefault[i].descricao}</p>                
             </div>
             `
-        }
-        )
     }
+    )
+}
