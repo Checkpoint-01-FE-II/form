b0504123fca8192308d088fbf208933eaaabb0a1
marcelo miyoshi <miyoshi_marcelo@hotmail.com>
2021-09-08 18:49:44 -0300

    musicas

--- js/game-page-default-onepiece.js
+++ js/game-page-default-onepiece.js
@@ -4,6 +4,9 @@ const $h1InfosNaTela = document.querySelector('.h1');
 const $h2InfosNaTela = document.querySelector('.h2');
 const $infosNaTela = document.querySelector('.infos-na-tela');
 
+const audio = document.getElementById("musica");
+audio.volume = 0.6;
+
 let hh = 0;
 let mm = 0;
 let ss = 0;
@@ -33,6 +36,7 @@ function start() {
         $infosNaTela.style.display='none';
     cron = setInterval(() => { timer(); }, tempo)
     document.getElementById("btniniciar").style.display = "none"
+    document.getElementById("musica").autoplay="true"
     document.getElementById("btnreiniciar").style.display = "block"},4000)
 }
 
