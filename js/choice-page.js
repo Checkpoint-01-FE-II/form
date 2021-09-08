5a298ccdb6b83f409972d27d8fa9ce5ba31bc60c
marcelonader <marcelo.m.nader@gmail.com>
2021-09-08 18:17:26 -0300

    reorganiza os arquivos

--- js/choice-page.js
+++ js/choice-page.js
@@ -4,15 +4,15 @@ const $select = document.querySelector('select')
 $btn.addEventListener('click', ()=>{
     if(document.querySelector('#default').checked){
         switch($select.value){
-            case 'onepiece':location.href = '../game-page-default-onepiece.html'
+            case 'onepiece':location.href = '../html/game-page-default-onepiece.html'
             break;
-            case 'marvel': location.href = '../game-page-default-marvel.html'
+            case 'marvel': location.href = '../html/game-page-default-marvel.html'
             break;
-            case 'personagens-aleatorios-1': location.href = '../game-page-default-aleatorios1.html'
+            case 'personagens-aleatorios-1': location.href = '../html/game-page-default-aleatorios1.html'
             break;
-            case 'personagens-aleatorios-2': location.href = '../game-page-default-aleatorios2.html'
+            case 'personagens-aleatorios-2': location.href = '../html/game-page-default-aleatorios2.html'
             break;
-            default: location.href = '../game-page-default.html'
+            default: location.href = '../html/game-page-default.html'
         }
         
     } else if(document.querySelector('#created').checked){
