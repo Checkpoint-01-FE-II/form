98fa61d4d616c69990b2cf5efb6538a1751c1961
marcelonader <marcelo.m.nader@gmail.com>
2021-09-07 21:50:51 -0300

    faz alterações na game-page-created

--- js/game-page-created.js
+++ js/game-page-created.js
@@ -1,14 +1,92 @@
-const $header= document.querySelector('header')
-const $containerCartas = document.querySelector('.container-cartas')
+const $header = document.querySelector('header');
+const $containerCartas = document.querySelector('.container-cartas');
+const $h1InfosNaTela = document.querySelector('.h1');
+const $h2InfosNaTela = document.querySelector('.h2');
+const $infosNaTela = document.querySelector('.infos-na-tela');
 
-
-let tempo = '00:00:00';
+let hh = 0;
+let mm = 0;
+let ss = 0;
+let cron;
+let tempo = 1000;/*quantos milessimos equivalem a 1 seg*/
 let paresEncontrados = 0;
 
+
+function contador (){
+    $infosNaTela.style.display='block';
+    setTimeout(()=>{
+        $h1InfosNaTela.innerHTML='3'
+    }, 1000)
+    setTimeout(()=>{
+        $h1InfosNaTela.innerHTML='2'
+    }, 2000)
+    setTimeout(()=>{
+        $h1InfosNaTela.innerHTML='1'
+    }, 3000)
+}
+function start() {
+    const $modal = document.querySelector('.modal');
+    $modal.style.display='none';
+    contador();
+    setTimeout(()=>{
+        $h1InfosNaTela.style.display='none';
+        $infosNaTela.style.display='none';
+    cron = setInterval(() => { timer(); }, tempo)
+    document.getElementById("btniniciar").style.display = "none"
+    document.getElementById("btnreiniciar").style.display = "block"},4000)
+}
+
+function pausar() {
+    
+}
+function recomecar() {
+    // cron = clearInterval
+    // hh = 0;
+    // mm = 0;
+    // ss = 0;
+
+    // embaralharCartas()
+    // cron = setInterval(() => { timer(); }, tempo)
+    location.href='../game-page-default.html'
+    // document.getElementById("btnreiniciar").style.display = "block"
+       
+}
+
+function pausar() {
+    clearInterval(cron);
+
+}
+// function recomecar() {
+//     document.getElementById("btniniciar").style.display = "block"
+//     document.getElementById("btnreiniciar").style.display = "none"
+      
+//     clearInterval(cron);
+//     hh = 0;
+//     mm = 0;
+//     ss = 0;
+    
+// }
+function timer() {
+    ss++;
+
+    if (ss == 60) {
+        ss = 0;
+        mm++;
+        if (mm == 60) {
+            mm = 0;
+            hh++;
+        }
+    }
+
+    let formato = "Tempo : " + (hh < 10 ? "0" + hh : hh) + ":" + (mm < 10 ? "0" + mm : mm) + ":" + (ss < 10 ? "0" + ss : ss)
+    document.getElementById("Iniciar").innerText = formato
+}
+
 $header.insertAdjacentHTML("beforeend", `
-    <h6>Tempo: ${tempo}</h6>
-    <h6>Pares encontrados: ${paresEncontrados}/6</h6>
+    <h6 id="Iniciar">Tempo : 00:00:00</h6>
+    <h6 class='pares-encontrados'>Pares encontrados: ${paresEncontrados}/6</h6>
 `);
+
 
 let storagelistaDeCartas = JSON.parse(localStorage.getItem("listaDeCartas"));
 
@@ -23,10 +101,11 @@ const embaralharCartas = (arr)=>{
 
 storagelistaDeCartas = embaralharCartas(storagelistaDeCartas);
 
-for(let i=0; i<storagelistaDeCartas.length;i++){
-    storagelistaDeCartas[i].id=`carta${i}`
-    $containerCartas.insertAdjacentHTML('beforeend', 
-    `
+
+for (let i = 0; i < storagelistaDeCartas.length; i++) {
+    storagelistaDeCartas[i].id = `carta${i}` //adiciona o id das cartas dinamicamente.
+    $containerCartas.insertAdjacentHTML('beforeend', //adiciona as cartas na tela.
+        `
         <div class='cartas' id=${storagelistaDeCartas[i].id}>
             <div class='div-frente-verso front'>
                 <img class='img-cartas' src='${storagelistaDeCartas[i].url}' alt="">
@@ -38,23 +117,78 @@ for(let i=0; i<storagelistaDeCartas.length;i++){
     `)
 }
 
-for(let i=0; i<storagelistaDeCartas.length;i++){
+let arrayDuasCartas = [];
 
-    document.querySelector("#carta" + i).addEventListener('click', function (){
+//adiciona um evento de click às cartas que adiciona/remove a classe flip aos elementos que possuem a classe "div-frente-verso"
+for (let i = 0; i < storagelistaDeCartas.length; i++) {
+
+    document.querySelector("#carta" + i).addEventListener('click', function () {
         let faces = this.getElementsByClassName('div-frente-verso')
-        faces[0].classList.toggle('flip');
-        faces[1].classList.toggle('flip');
-        document.querySelector('.section-descricao').innerHTML =
-        `
-        <img class='img-descricao'
-        src=${storagelistaDeCartas[i].url}
-        alt="">
-        <div>
-            <h3>${storagelistaDeCartas[i].titulo}<h3>
-            <p>${storagelistaDeCartas[i].descricao}</p>                
-        </div>
-        `
+        if (faces[0].classList.contains('flip') && faces[1].classList.contains('flip')) {
+            return
+        } else {
+            faces[0].classList.toggle('flip');
+            faces[1].classList.toggle('flip');
+
+            document.querySelector('.section-descricao').innerHTML =
+                `
+            <img class='img-descricao'
+            src="${storagelistaDeCartas[i].url}"
+            alt="">
+            <div>
+                <h3>${storagelistaDeCartas[i].titulo}<h3>
+                <p>${storagelistaDeCartas[i].descricao}</p>                
+            </div>
+            `
+            verificaPar(i)
+        }
     }
     )
+}
+
+
+
+function verificaPar(i) {
+
+    arrayDuasCartas.length == 2 ? arrayDuasCartas = [] : '';
+
+    if (arrayDuasCartas.length == 0) {
+        arrayDuasCartas.push(storagelistaDeCartas[i])
+        console.log(arrayDuasCartas)
+    } else if (arrayDuasCartas.length == 1) {
+        arrayDuasCartas.push(storagelistaDeCartas[i])
+        let idElemento1 = `#${arrayDuasCartas[0].id}`;
+        let idElemento2 = `#${arrayDuasCartas[1].id}`;
+        let faceElemento1 = document.querySelector(idElemento1).getElementsByClassName('div-frente-verso');
+        let faceElemento2 = document.querySelector(idElemento2).getElementsByClassName('div-frente-verso');
+        if (arrayDuasCartas[0].url == arrayDuasCartas[1].url) {
+            faceElemento1[0].style.opacity = '0.2';
+            faceElemento1[1].style.opacity = '0.2';
+            faceElemento2[0].style.opacity = '0.2';
+            faceElemento2[1].style.opacity = '0.2';
+
+            paresEncontrados++;
+
+            document.querySelector('.pares-encontrados').innerHTML = `Pares encontrados: ${paresEncontrados}/6`
+            setTimeout(() => {
+                if (paresEncontrados === 6) {
+                    $infosNaTela.style.display='block';
+                    $h2InfosNaTela.style.display='block';
+                    $h2InfosNaTela.innerHTML=`
+                    Fim de Jogo!
+                    Seu tempo foi de: ${(hh < 10 ? "0" + hh : hh)}:${(mm < 10 ? "0" + mm : mm)}:${(ss < 10 ? "0" + ss : ss)}.`
+                    pausar()}}, 1000)
+
+        } else {
+            arrayDuasCartas = [];
+            setTimeout(() => {
+                faceElemento1[0].classList.toggle('flip')
+                faceElemento1[1].classList.toggle('flip')
+                faceElemento2[0].classList.toggle('flip')
+                faceElemento2[1].classList.toggle('flip')
+                document.querySelector('.section-descricao').innerHTML = '';
+            }, 1200)
+        }
+    }
 }
 
